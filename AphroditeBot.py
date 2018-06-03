b070a91d252e44379627bd6a7942243b1c2528e8
Dameon Owen <dameonowen@yahoo.com>
2018-06-03 19:51:02 -0400

    Get the message formatting right.

--- AphroditeBot.py
+++ AphroditeBot.py
@@ -141,8 +141,10 @@ def handle_queue():
     elif command_message(queuedMsg):
         queuedMsg = queuedMsg.replace("@", "(at)")
         queuedMsg = queuedMsg.strip("ANNOUNCEMENT - ")
-        yield from ourBot.send_message(ourBot.get_channel(config.commandID), queuedMsg)
-        print("'"+queuedMsg+"'" +" sent to command channel\n\n")
+        cmdMsg = queuedMsg.split("\n")
+        yield from ourBot.send_message(ourBot.get_channel(config.commandID), cmdMsg[0])
+        yield from ourBot.send_message(ourBot.get_channel(config.commandID), cmdMsg[1])
+        print("'"+header+"'"+"\n'"+contents+"'\n"+" sent to command channel\n\n")
 
     elif general_message(queuedMsg):
         yield from ourBot.send_message(ourBot.get_channel(config.generalID), queuedMsg)
